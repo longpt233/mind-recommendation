4956e7f3bc11b9d4be4acfc5b1f3ffec25591717
Phan Thanh Long <driverofl01@gmail.com>
2022-01-01 21:20:07 +0700

    commit nrms trc khi chuyen ve ban cu

--- nrms/reco_utils/recommender/newsrec/models/nrms_entity.py
+++ nrms/reco_utils/recommender/newsrec/models/nrms_entity.py
@@ -1,5 +1,3 @@
-# Copyright (c) Microsoft Corporation. All rights reserved.
-# Licensed under the MIT License.
 import torch.nn.functional as F
 
 __all__ = ["NRMSModelEntity"]
@@ -13,10 +11,13 @@ class NRMSModelEntity(NRMSModel):
         super().__init__(hparams)
 
     def news_encoder(self, sequences_input):
+
         title, entity = sequences_input[:, :self.hparams.title_size], sequences_input[:, self.hparams.title_size:]
+        
         y = F.dropout(self.embedding_layer(title), p=self.hparams.dropout).transpose(0, 1)
+
         q = F.dropout(self.embedding_layer(entity), p=self.hparams.dropout).transpose(0, 1)
-        y = self.news_self_att(q, y, y)[0].transpose(0, 1)
+        y = self.news_self_att(q, y, y)[0].transpose(0, 1)   # lấy mỗi cái 0 là láy mỗi cái len 
         y = F.dropout(y, p=self.hparams.dropout)
         y = self.news_att_layer(y)
         return y
